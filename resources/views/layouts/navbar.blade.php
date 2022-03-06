d3192522b4353de051d59bc99f3767180026ecdf
Nikola-mkd <nikolad150@gmail.com>
2022-03-06 13:57:35 +0100

    logout lg

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -11,28 +11,41 @@
                 @if(Auth::user()->image != null)
                 <ul class="navbar-nav ms-auto font-1rem">
                     <li class="nav-item ">
-                        <a class="nav-link navbar-responsive @if((Request::is('projects/*'))|| (Request::is('applicants/*'))) active @endif" aria-current="page" href="/projects/my-projects">My projects</a>
+                        <a class="nav-link navbar-responsive {{ Request::is('projects/my-projects') ? 'active' : '' }}" aria-current="page" href="/projects/my-projects">My projects</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link navbar-responsive {{ Request::is('applications/*') ? 'active' : '' }}" href="/applications/my-applications">My appclications</a>
+                        <a class="nav-link navbar-responsive " href="/applications/my-applications">My appclications</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link navbar-responsive {{ Request::is('profile/*') ? 'active' : '' }}" href="/profile/my-profile">My profile</a>
-                    </li>
-                    <li class="nav-item">
-
+                        <a class="nav-link navbar-responsive {{ Request::is('profile/my-profile') ? 'active' : '' }}" href="/profile/my-profile">My profile</a>
                     </li>
                 </ul>
                 @endif
-                @if(Auth::user()->image != null)
-                <a class="nav-link " href="/profile/my-profile"><img class="avatar" src="{{ asset('avatars/' . Auth::user()->image) }}" alt="..."></a>
+                @if(Auth::user()->image == null)
+                <a class="nav-link d-grid gap-2 d-md-flex justify-content-md-end" href="/profile/my-profile"><img class="avatar" src="{{ asset('avatars/default_avatar.png') }}" alt="..."></a>
+                @else
+                <!-- <a class="nav-link " href="/profile/my-profile"><img class="avatar" src="{{ asset('avatars/' . Auth::user()->image) }}" alt="..."></a> -->
+                <div class="btn-group dropstart">
+                    <!-- <button type="button" class="btn btn-secondary dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                        Dropstart
+                    </button> -->
+                    <a class="nav-link " href="/profile/my-profile" data-bs-toggle="dropdown" aria-expanded="false"><img class="avatar dropdown-toggle" src="{{ asset('avatars/' . Auth::user()->image) }}" alt="..."></a>
+                    <ul class="dropdown-menu mt-5">
+                        <li class="dropdown-item ">{{Auth::user()->email}}</li>
+                        <li>
+                            <hr class="dropdown-divider">
+                        </li>
+                        <li><a class="dropdown-item" href="/profile/my-profile">Edit Profile</a></li>
+                        <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </li>
+                    </ul>
+                </div>
                 @endif
             </div>
         </div>
-        @if(Auth::user()->image == null)
-        <div class="d-flex flex-row-reverse bd-highlight">
-            <a class="bd-highlight" href="/profile/my-profile"><img class="avatar" src="{{ asset('avatars/default_avatar.png') }}" alt="..."></a>
-        </div>
-        @endif
     </nav>
 </header>
